ef2663d7b58709c5a5fe4aa64082cd3a638727f9
kirti8192 <kirti8192@kirtis-MacBook-Pro.local>
2024-08-13 17:08:29 -0700

    scatter plot added, dataset split

--- perceptron/perceptron_binary_classifier.py
+++ perceptron/perceptron_binary_classifier.py
@@ -1,5 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
+import mglearn
+from IPython.display import display
 
 # read data from csv 
 raw_data = pd.read_csv('datasets/mushroom_cleaned.csv')
 
+# split dataframe into training data and classification
+train_data = raw_data.iloc[:,:-1]
+label_data = raw_data.iloc[:,-1]
+
+# create a scatter matrix from the dataframe, color by y_train
+pd.plotting.scatter_matrix(train_data, c=label_data, figsize=(15, 15),
+                           marker='o', hist_kwds={'bins': 20}, s=60,
+                           alpha=.8, cmap=mglearn.cm3)
+plt.show()
